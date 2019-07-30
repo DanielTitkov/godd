b8cd91ffa133b3d2b4530af734263cc9d160fa19
DanielTitkov <mytasmic@gmail.com>
2019-07-30 17:09:34 +0300

    created getFileSize function

--- godd.go
+++ godd.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 )
+
+func getFileLength(file *os.File) int64 {
+	stat, err := file.Stat()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return stat.Size()
+}
 
 func main() {
 	fmt.Println("foo")
+	file, err := os.Open("foo.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fl := getFileLength(file)
+	fmt.Println(fl)
 }
